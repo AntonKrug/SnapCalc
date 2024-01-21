d9701fa8ea4daf0480a44be460dbc7477ac56ee0
Anton Krug <antonkrug@antonkrug.eu>
2024-01-21 21:45:20 +0000

    ShutterSpeed is done as CaroselView as well

--- SnapCalc/MainPage.xaml.cs
+++ SnapCalc/MainPage.xaml.cs
@@ -17,21 +17,92 @@ namespace SnapCalc
             Iso1.SelectedIndex      = 0;
             Iso2.SelectedIndex      = 0;
 
+            // ND filters
             List<FilterItem> itemsFilter =
-            [
-                new() { Name = "ND0",    Stops = 0,               Description = "no filter" },
-                new() { Name = "ND2",    Stops = 1,               Description = "1 stop variable" },
-                new() { Name = "ND4",    Stops = 2,               Description = "2 stops" },
-                new() { Name = "ND8",    Stops = 3,               Description = "3 stops" },
-                new() { Name = "ND16",   Stops = 4,               Description = "4 stops variable" },
-                new() { Name = "ND32",   Stops = 5,               Description = "5 stops variable" },
-                new() { Name = "ND64",   Stops = 6,               Description = "6 stops" },
-                new() { Name = "ND1000", Stops = Math.Log2(1000), Description = "~10 stops" },
-            ];
+                [
+                    new() { Name = "ND1",    Stops = 0,               Description = "no filter" },
+                    new() { Name = "ND2",    Stops = 1,               Description = "1 stop variable" },
+                    new() { Name = "ND4",    Stops = 2,               Description = "2 stops" },
+                    new() { Name = "ND8",    Stops = 3,               Description = "3 stops" },
+                    new() { Name = "ND16",   Stops = 4,               Description = "4 stops variable" },
+                    new() { Name = "ND32",   Stops = 5,               Description = "5 stops variable" },
+                    new() { Name = "ND64",   Stops = 6,               Description = "6 stops" },
+                    new() { Name = "ND1000", Stops = Math.Log2(1000), Description = "~10 stops" },
+                ];
+
             CurrentNd.ItemsSource = itemsFilter;
             NewNd1.ItemsSource = itemsFilter;
-            NewNd1.Position = 7; // the ND1000 is default position for this filter
             NewNd2.ItemsSource = itemsFilter;
+            // The last filter option is the default position for NewNd1
+            NewNd1.Position = itemsFilter.Count-1; 
+
+            // Shutter speed
+            List<ShutterSpeedItem> itemsShutterSpeed =
+                [
+                    new ShutterSpeedItem("16000"),
+                    new ShutterSpeedItem("12800"),
+                    new ShutterSpeedItem("10000"),
+                    new ShutterSpeedItem("8000"),
+                    new ShutterSpeedItem("6400"),
+                    new ShutterSpeedItem("5000"),
+                    new ShutterSpeedItem("4000"),
+                    new ShutterSpeedItem("3200"),
+                    new ShutterSpeedItem("2500"),
+                    new ShutterSpeedItem("2000"),
+                    new ShutterSpeedItem("1600"),
+                    new ShutterSpeedItem("1250"),
+                    new ShutterSpeedItem("1000"),
+                    new ShutterSpeedItem("800"),
+                    new ShutterSpeedItem("640"),
+                    new ShutterSpeedItem("500"),
+                    new ShutterSpeedItem("400"),
+                    new ShutterSpeedItem("320"),
+                    new ShutterSpeedItem("250"),
+                    new ShutterSpeedItem("200"),
+                    new ShutterSpeedItem("160"),
+                    new ShutterSpeedItem("125"),
+                    new ShutterSpeedItem("100"),
+                    new ShutterSpeedItem("80"),
+                    new ShutterSpeedItem("60"),
+                    new ShutterSpeedItem("50"),
+                    new ShutterSpeedItem("40"),
+                    new ShutterSpeedItem("30"),
+                    new ShutterSpeedItem("25"),
+                    new ShutterSpeedItem("20"),
+                    new ShutterSpeedItem("15"),
+                    new ShutterSpeedItem("13"),
+                    new ShutterSpeedItem("10"),
+                    new ShutterSpeedItem("8"),
+                    new ShutterSpeedItem("6"),
+                    new ShutterSpeedItem("5"),
+                    new ShutterSpeedItem("4"),
+                    new ShutterSpeedItem("0\"3"),
+                    new ShutterSpeedItem("0\"4"),
+                    new ShutterSpeedItem("0\"5"),
+                    new ShutterSpeedItem("0\"6"),
+                    new ShutterSpeedItem("0\"8"),
+                    new ShutterSpeedItem("1\""),
+                    new ShutterSpeedItem("1\"3"),
+                    new ShutterSpeedItem("1\"6"),
+                    new ShutterSpeedItem("2\""),
+                    new ShutterSpeedItem("2\"5"),
+                    new ShutterSpeedItem("3\"2"),
+                    new ShutterSpeedItem("4\""),
+                    new ShutterSpeedItem("5\""),
+                    new ShutterSpeedItem("6\""),
+                    new ShutterSpeedItem("8\""),
+                    new ShutterSpeedItem("10\""),
+                    new ShutterSpeedItem("13\""),
+                    new ShutterSpeedItem("15\""),
+                    new ShutterSpeedItem("20\""),
+                    new ShutterSpeedItem("25\""),
+                    new ShutterSpeedItem("30\"")
+                ];
+
+            CurrentShutter.ItemsSource = itemsShutterSpeed;
+
+            // Default possition is the 1/100 shutter speed
+            CurrentShutter.Position = itemsShutterSpeed.FindIndex(i => i.Abbreviation == "100"); 
         }
 
 
@@ -77,39 +148,15 @@ namespace SnapCalc
         }
 
 
-        private static double ParseShutterSpeed(string shutterSpeed)
-        {
-            if (shutterSpeed.Contains('/'))
-            {
-                //  1/250s
-                return 1.0 / double.Parse(shutterSpeed[2..^1]);
-            }
-
-            // 1.3s
-            return double.Parse(shutterSpeed[..^1]);
-        }
-
-
-        private static double GetShutterSpeed(string current)
-        {
-            if (current is null) 
-            { 
-                return 1.0; 
-            }
-
-            return ParseShutterSpeed(current);
-        }
-
-
         private double GetEv()
         {
             // https://en.wikipedia.org/wiki/Exposure_value
-            var N = GetAperture((string)Aperture1.SelectedItem);
-            var S = GetIso((string)Iso1.SelectedItem);
-            var t = GetShutterSpeed((string)Shutter1.SelectedItem);
+            var aperture = GetAperture((string)Aperture1.SelectedItem);
+            var iso = GetIso((string)Iso1.SelectedItem);
+            double shutterTime = ((ShutterSpeedItem)CurrentShutter.CurrentItem)?.Seconds ?? 1;
 
-            var EV = Math.Log2((N * N) / t) - Math.Log2(S / 100);
-            return EV;
+            var ev = Math.Log2(aperture * aperture / shutterTime) - Math.Log2(iso / 100);
+            return ev;
         }
 
         private double CalculateShutterSpeed(double ev)
@@ -132,7 +179,7 @@ namespace SnapCalc
 
         private static double GetNdFilter(CarouselView filter)
         {
-            return ((FilterItem)(filter.CurrentItem))?.Stops ?? 0;
+            return ((FilterItem)filter.CurrentItem)?.Stops ?? 0;
         }
 
 
---
+++ SnapCalc/Models/ShutterSpeedItem.cs
@@ -0,0 +1,33 @@
+﻿namespace SnapCalc.Models
+{
+    internal class ShutterSpeedItem
+    {
+
+        public string Abbreviation { get; }
+
+
+        public double Seconds { get; }
+
+
+        public string SecondsFormated => $"{Seconds:F6}s";
+
+
+        public ShutterSpeedItem(string abbreviation) 
+        { 
+            Abbreviation = abbreviation;
+            
+            if (abbreviation.Contains('"'))
+            {
+                // The " symbol represnts fraction point .
+                Seconds = double.Parse(abbreviation.Replace('"', '.'));
+            } 
+            else
+            {
+                // All other numbers x are actually 1 / x
+                Seconds = 1 / double.Parse(abbreviation);
+            }
+        }
+
+
+    }
+}
